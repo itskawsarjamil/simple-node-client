c958be5dfa92c2ae52c8f0976d055254cc2bb8b9
Kawsar Jamil <68264633+itskawsarjamil@users.noreply.github.com>
2023-01-27 19:30:53 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    fetch('http://localhost:5000/users')
+      .then(res => res.json())
+      .then(data => setUsers(data));
+  }, []);
+
+  const handleSubmit = (e) => {
+    const name = e.target.name.value;
+    const passion = e.target.passion.value;
+
+    const user = { name, passion };
+    fetch('http://localhost:5000/users', {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify(user)
+    })
+      .then(res => res.json())
+      .then(data => {
+        console.log("post fetch data:", data);
+        const newUsers = [...users, data];
+        setUsers(newUsers);
+      })
+      .catch(err => console.error(err));
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <form onSubmit={handleSubmit}>
+          <br />
+          <input type="text" name='name' placeholder='name' required />
+          <input type="text" required name='passion' placeholder='passion' />
+          <br />
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+      {
+        users.map((user, idx) => (
+          <p style={{ border: "2px solid red" }} key={idx}>name: {user.name}, Passion: {user.passion}</p>
+        ))
+      }
     </div>
   );
 }
